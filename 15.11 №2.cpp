6a74834ed25bae055250df3a518a1ed527b2a827
DIRECTOR228PLAY <150294748+DIRECTOR228PLAY@users.noreply.github.com>
2023-11-15 11:09:30 +0300

    Add files via upload

---
+++ 15.11 №2.cpp
@@ -0,0 +1,19 @@
+﻿// 15.11 №2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "");
+    int arr[] = { 1, 2, 3, 4, 5 };
+    int sum = 0;
+
+    for (int i = 0; i < 5; i++) {
+        sum += arr[i];
+    }
+
+     cout << "Сумма всех элементов массива: " << sum << std::endl;
+
+    return 0;
+}
